93c6a166118135bd49c007526518b20b9b3f6ac4
unjinjang <alpsboy1996@gmail.com>
2022-05-08 18:40:13 +0900

    feat: my solution added

--- Matrix/Leetcode 48. Rotate Image.js
+++ Matrix/Leetcode 48. Rotate Image.js
@@ -1,3 +1,31 @@
+// My solution
+// I should not allocate another 2D matrix to solve the question
+// But I broke the rule
+
+var rotate = function (matrix) {
+  let copy = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    copy.push([...matrix[i]]);
+  }
+
+  for (let i = 0; i < copy.length; i++) {
+    let temp = [];
+
+    for (let j = copy.length - 1; j >= 0; j--) {
+      temp.push(copy[j][i]);
+    }
+
+    for (let k = 0; k < temp.length; k++) {
+      matrix[i][k] = temp[k];
+    }
+  }
+
+  return matrix;
+};
+
+// Most Voted Answer
+
 const rotate = (matrix) => {
   matrix = matrix.reverse();
 
