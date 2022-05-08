83ac0c019ae2b27e12cae4cc4f7100badfd60532
unjinjang <alpsboy1996@gmail.com>
2022-05-08 18:38:24 +0900

    feat: backtracking algorithm directory created

---
+++ Backtracking/Leetcode 78. Subsets.js
@@ -0,0 +1,21 @@
+// Cascading Solution
+
+var subsets = function (nums) {
+  const ans = [[]];
+
+  for (let i = 0; i < nums.length; i++) {
+    let temp = [];
+
+    for (let j = 0; j < ans.length; j++) {
+      const emptySubset = [...ans[j]];
+
+      emptySubset.push(nums[i]);
+
+      temp.push(emptySubset);
+    }
+
+    ans.push(...temp);
+  }
+
+  return ans;
+};
