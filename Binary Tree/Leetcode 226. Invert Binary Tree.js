42034fe49f3b3b1c3cc9c827559a2122453e5ebf
unjinjang <alpsboy1996@gmail.com>
2022-05-08 18:32:18 +0900

    invert binary tree algorithm

---
+++ Binary Tree/Leetcode 226. Invert Binary Tree.js
@@ -0,0 +1,12 @@
+var invertTree = function (root) {
+  if (!root) return root;
+
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  if (root.left) invertTree(root.left);
+  if (root.right) invertTree(root.right);
+
+  return root;
+};
