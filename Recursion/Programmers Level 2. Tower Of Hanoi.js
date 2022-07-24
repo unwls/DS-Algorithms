8eb4a3a88b4be6de15853c982dc2aa0aed38db7c
unjinjang <alpsboy1996@gmail.com>
2022-07-24 16:25:41 +0900

    feat: add tower of hanoi algorithm solution

---
+++ Recursion/Programmers Level 2. Tower Of Hanoi.js
@@ -0,0 +1,16 @@
+function solution(n) {
+  return helperFunction(n, 1, 3, 2);
+}
+
+const helperFunction = (disk, departure, destination, waypoint) => {
+  if (disk === 1) return [[departure, destination]];
+
+  return [
+    ...helperFunction(disk - 1, departure, waypoint, destination),
+    ...[[departure, destination]],
+    ...helperFunction(disk - 1, waypoint, destination, departure),
+  ];
+};
+
+// Question link: https://school.programmers.co.kr/learn/courses/30/lessons/12946
+// Useful link: https://shoark7.github.io/programming/algorithm/tower-of-hanoi
